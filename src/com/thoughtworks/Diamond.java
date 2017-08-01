391bec2405c2613bf619988655d469c9f6f51a51
Izael Effemberg <ieffembe@thoughtworks.com>
2017-08-01 10:55:48 -0300

    second  commit

--- src/com/thoughtworks/Diamond.java
+++ src/com/thoughtworks/Diamond.java
@@ -2,7 +2,36 @@ package com.thoughtworks;
 
 public class Diamond {
 
-    public void printDiamond(){
+    public void printDiamond(int numberOfAsterisks){
+        for (int i = numberOfAsterisks * 2; i > 1; i -= 2) {
+            for (int j = (numberOfAsterisks - (i / 2) + 1); j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int j = i; j > 1; j--) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+    public void printDiamondWithName(String name, int numberOfAsterisks){
+
+        createTopOfDiamond(numberOfAsterisks - 1);
+        System.out.println(name);
+        printDiamond(numberOfAsterisks - 1);
+
+    }
+
+    private void createTopOfDiamond(int numberOfAsterisks){
+        for (int i = 1; i < numberOfAsterisks * 2; i += 2) {
+            for (int j = 1; j < (numberOfAsterisks - (i / 2) + 1); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
 
     }
 
